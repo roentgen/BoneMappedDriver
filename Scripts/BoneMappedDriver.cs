ed883dab63d3d5993337f0e49efd8064f829119f
roentgen <fukasawa666@gmail.com>
2022-05-25 16:28:19 +0900

    fixed name of parameters. fixed linear correction

--- Scripts/BoneMappedDriver.cs
+++ Scripts/BoneMappedDriver.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 
 public class BoneMappedDriver : MonoBehaviour
 {
-        public HumanBodyBones Drived;
-        public GameObject Referer;
+        public HumanBodyBones DeformingBone;
+        public GameObject Reference;
         public bool Absolute; 
         public bool Clamp; // whether a dp's clamped to positive [0.f - 1.f]
         public bool Linear; // use an angle (uniformed to PI/2) linear correction  instead of dot
@@ -48,16 +48,16 @@ public class BoneMappedDriver : MonoBehaviour
                         Debug.LogWarning("Warning:  there are no BlendShapes in SkinnedMeshComponent" );
                 if (Anim == null) 
                         Debug.LogError("Error: Component owner does not have Animator component." );
-                if (Referer == null /* || Drived == None*/) 
-                        Debug.LogError("Error: Referer and Drived must be set." );
+                if (Reference == null /* || DeformingBone == None*/) 
+                        Debug.LogError("Error: Reference and DeformingBone must be set." );
         }
 
         // Update is called once per frame
         void Update()
         {
-                if (Anim == null || Referer == null || /* Drived == None || */ Mesh == null || BlendShapeIndices.Length == 0)
+                if (Anim == null || Reference == null || /* DeformingBone == None || */ Mesh == null || BlendShapeIndices.Length == 0)
                         return;
-                var b = Anim.GetBoneTransform(Drived);
+                var b = Anim.GetBoneTransform(DeformingBone);
                 var v = new Vector3(0.0f, 0.1f, 0.0f); // a mecanim bone shall be Y-axis bone
                 Func< Transform, Vector3 > head = (tr) => tr.rotation * v;
 
@@ -73,21 +73,21 @@ public class BoneMappedDriver : MonoBehaviour
                                 "\n              localRotation: " + to_str(t.localRotation.eulerAngles);
                 };
         
-                var boundref = head(Referer.transform);
+                var boundref = head(Reference.transform);
                 var boundbone = head(b);
                 float dot = Vector3.Dot(boundref.normalized, boundbone.normalized);
                 if (DebugDraw) {
-                        Debug.Log("dot: " + dot + " ref:"  + to_str(boundref) + " bone:" + to_str(boundbone));
+                        //Debug.Log("dot: " + dot + " (" + (float)((Math.PI / 2.0 - Math.Acos(dot)) / (Math.PI / 2.0)) + " rad) ref:"  + to_str(boundref) + " bone:" + to_str(boundbone));
                         // Debug.Log("Ref:");
-                        // Debug.Log(dump(Referer.transform));
+                        // Debug.Log(dump(Reference.transform));
                         // Debug.Log("Bone:");
                         // Debug.Log(dump(b));
                         Debug.DrawLine(b.position, b.position + head(b), Color.yellow);
-                        Debug.DrawLine(Referer.transform.position, Referer.transform.position + head(Referer.transform), Color.magenta);
+                        Debug.DrawLine(Reference.transform.position, Reference.transform.position + head(Reference.transform), Color.magenta);
                 }
                 dot = Absolute ? Math.Abs(dot) : dot;
                 dot = Clamp ? Math.Max(Math.Min(dot, 1.0f), 0.0f) : dot;
-                dot = Linear ? (float)(Math.Acos(dot) / (Math.PI / 2.0)) : dot;
+                dot = Linear ? (float)((Math.PI / 2.0 - Math.Acos(dot)) / (Math.PI / 2.0)) : dot;
                 float e = 0.0f;
                 int last = -1;
                 for (var i = 0; i < BlendShapes.Length; i ++)
